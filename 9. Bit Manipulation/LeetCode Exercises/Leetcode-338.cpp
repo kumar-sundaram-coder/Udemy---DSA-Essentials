8474b8570ca0445ed55ac65a4912627fc55556eb
kumar-sundaram-coder <sandyroshan2000@gmail.com>
2024-03-01 13:49:05 +0530

    Leetcode ques another approach added

--- 9. Bit Manipulation/LeetCode Exercises/Leetcode-338.cpp
+++ 9. Bit Manipulation/LeetCode Exercises/Leetcode-338.cpp
@@ -42,3 +42,29 @@ public:
         return ans;
     }
 };
+
+// Method - 3 | counting the number of ones seperately with the help of custom fxn | hack approach | using n & (n-1) -> this will decrease the value of significant bit present in n -> from least significant side
+class Solution
+{
+public:
+    int countNumberOfOnes(int n)
+    {
+        int ans = 0;
+        while (n != 0)
+        {
+            n = n & (n - 1); // this will keep on decreasing the count of 1 from the
+                             // least significant bit side(right to left side)
+            ans++;           // incerase the value of ans
+        }
+        return ans;
+    }
+    vector<int> countBits(int n)
+    {
+        vector<int> ans;
+        for (int i = 0; i < n + 1; i++)
+        {
+            ans.push_back(countNumberOfOnes(i));
+        }
+        return ans;
+    }
+};
